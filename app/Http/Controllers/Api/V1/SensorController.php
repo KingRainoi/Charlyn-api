ab066d25f15c747ae92d2edc85318f91a8d3af95
Rubén Gamboa <bezyex@gmail.com>
2023-09-27 17:54:20 -0600

    Creación de modelo de Device

--- app/Http/Controllers/Api/V1/SensorController.php
+++ app/Http/Controllers/Api/V1/SensorController.php
@@ -6,28 +6,30 @@ use App\Models\Sensor;
 use App\Http\Requests\StoreSensorRequest;
 use App\Http\Requests\UpdateSensorRequest;
 use App\Http\Controllers\Controller;
-use App\Http\Resources\SensorResource;
 use Illuminate\Support\Facades\Http;
 
 class SensorController extends Controller
 {
     /**
+     * Realizar la solicitud a la API externa de PRTG y manejar la respuesta.
+     */
+    private function makeApiSensorRequest()
+    {
+        $response = Http::withoutVerifying()->get('https://127.0.0.1/api/table.json?content=sensors&columns=device,sensor,status&username=root&password=prtgadmin');
+
+        if ($response->successful()) {
+            return response()->json($response->json());
+        } else {
+            return response()->json(['message' => 'Error al obtener datos de la API externa de PRTG'], 500);
+        }
+    }
+
+    /**
      * Display a listing of the resource.
      */
     public function index()
     {
-        // Realizar la solicitud a la API externa de PRTG
-        // $response = Http::withoutVerifying()->get('https://127.0.0.1/api/table.json?content=sensors&columns=sensor&username=root&password=prtgadmin');
-        $response = Http::withoutVerifying()->get('https://127.0.0.1/api/table.json?content=sensors&columns=device,sensor,status&username=root&password=prtgadmin');
-
-        // Verificar si la solicitud fue exitosa (código de respuesta 200)
-        if ($response->successful()) {
-            // Obtener los datos de la respuesta de la API externa y devolverlos como JSON
-            return response()->json($response->json());
-        } else {
-            // Manejar el caso en que la solicitud a la API externa de PRTG falla
-            return response()->json(['message' => 'Error al obtener datos de la API externa de PRTG'], 500);
-        }
+        return $this->makeApiSensorRequest();
     }
 
     /**
@@ -43,13 +45,7 @@ class SensorController extends Controller
      */
     public function show(Sensor $sensor)
     {
-        $response = Http::withoutVerifying()->get('https://127.0.0.1/api/table.json?content=sensors&columns=device,sensor,status&username=root&password=prtgadmin');
-
-        if ($response->successful()) {
-            return response()->json($response->json());
-        } else {
-            dd('Error en la solicitud a la API externa: ' . $response->status());
-        }
+        return $this->makeApiSensorRequest();
     }
 
     /**
